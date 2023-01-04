9256298589b40ad5dccd6c8b2ff2ba12d5227838
Leonel Huachaca <leonel.huachacav@gmail.com>
2023-01-04 10:10:34 -0500

    add design home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 
+interface tarea {
+  nombre: string;
+  descripcion: string;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-
+  tareas: tarea[] = [
+    {
+      nombre: 'Aprender Angular',
+      descripcion: 'Estudiar formularios',
+    },
+  ];
+  guardarTarea(){
+    
+  }
 }
