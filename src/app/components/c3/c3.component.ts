f13d984e64c52b5159823cea6a26eafc98d4c48f
Leonel Huachaca <leonel.huachacav@gmail.com>
2023-01-04 10:11:44 -0500

    add design home page

--- src/app/components/c3/c3.component.ts
+++ src/app/components/c3/c3.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
-
+import { Component, Output, EventEmitter } from '@angular/core';
+interface tarea {
+  nombre: string;
+  descripcion: string;
+}
 @Component({
   selector: 'app-c3',
   templateUrl: './c3.component.html',
-  styleUrls: ['./c3.component.css']
+  styleUrls: ['./c3.component.css'],
 })
 export class C3Component {
+  nombre!: string;
+  descripcion!: string;
 
+  @Output() responseTarea: EventEmitter<tarea> = new EventEmitter();
+
+  guardar() {
+    const newTarea: tarea = {
+      nombre: this.nombre,
+      descripcion: this.descripcion,
+    };
+    this.responseTarea.emit(newTarea);
+  }
 }
--- src/app/components/c4/c4.component.ts
+++ src/app/components/c4/c4.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
+interface tarea {
+  nombre: string;
+  descripcion: string;
+}
 @Component({
   selector: 'app-c4',
   templateUrl: './c4.component.html',
-  styleUrls: ['./c4.component.css']
+  styleUrls: ['./c4.component.css'],
 })
 export class C4Component {
-
+  @Input() tareas: tarea[] = [];
 }
