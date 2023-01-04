a88e5986eafcb092b0af531fd772ebe2321bf845
Leonel Huachaca <leonel.huachacav@gmail.com>
2023-01-04 09:29:41 -0500

    add structured project

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { C1Component } from './c1/c1.component';
+import { C2Component } from './c2/c2.component';
+import { C3Component } from './c3/c3.component';
+import { C4Component } from './c4/c4.component';
+
+@NgModule({
+  declarations: [C1Component, C2Component, C3Component, C4Component],
+  imports: [CommonModule],
+  exports: [C1Component, C2Component, C3Component, C4Component],
+})
+export class ComponentsModule {}
